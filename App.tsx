15a09c464dd67b9ad12d5f2451f7252468553c3e
aleucilima <aleuci.lima@gmail.com>
2022-05-26 23:04:20 -0300

    Rent-x rent a car

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  return (
+    <Home />
+  );
+}

6414fc3086b618c247ff4b039f822d4f459f1945
aleucilima <aleuci.lima@gmail.com>
2022-05-29 13:05:11 -0300

    Update CarDetails screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
   Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
 } from './styles';
 
 export function CarDetails() {
   return (
     <Container>
+      <StatusBar style="auto" />
       <Header>
-        <BackButton
-          onPress={() => {}}
-        />
+        <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider 
+          imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Fiat</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 40,00</Price>
+          </Rent>
+        </Details>
+      </Content>
     </Container>
   );
 }

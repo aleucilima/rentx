0622a6a9d86c99856ce3409f76ee86fbc982c0a7
aleucilima <aleuci.lima@gmail.com>
2022-05-29 13:28:33 -0300

    Update CarDetails screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,6 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Accessory';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -16,6 +24,8 @@ import {
   Rent,
   Period,
   Price,
+  Accessories,
+  About,
 } from './styles';
 
 export function CarDetails() {
@@ -44,6 +54,24 @@ export function CarDetails() {
             <Price>R$ 40,00</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Acessory name="380Km/h" icon={speedSvg}/>
+          <Acessory name="3.2s" icon={accelerationSvg}/>
+          <Acessory name="800 HP" icon={forceSvg}/>
+          <Acessory name="Gasolina" icon={gasolineSvg}/>
+          <Acessory name="Auto" icon={exchangeSvg}/>
+          <Acessory name="2 pessoas" icon={peopleSvg}/>
+
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário 
+          touro de lide indultadona praça Real Maestranza 
+          de Sevilla. É um belíssimo carro para quem gosta 
+          de acelerar.
+        </About>
+
       </Content>
     </Container>
   );

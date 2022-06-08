694f3142627d634364da8da1317e333b76273499
aleucilima <aleuci.lima@gmail.com>
2022-06-07 21:00:57 -0300

    update: listing user schedules

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { CarDTO } from '../../dtos/CarDTO';
+import api from '../../services/api';
 
 import {
   Container,
 } from './styles';
 
 export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchCars() {
+    await api.get(`/schedules_bycars?user_id=${1}`)
+    .then(response => {
+      setCars(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }
+
+  useEffect(() => {
+    fetchCars().then(() => {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <Container>
 

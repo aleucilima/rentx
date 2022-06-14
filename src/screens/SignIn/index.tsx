0dbb06a37ec15cec20c329c86ea925459f301a2b
aleucilima <aleuci.lima@gmail.com>
2022-06-14 20:38:55 -0300

    update: create SignIn screen.

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+  Container,
+  Header,
+  Title,
+} from './styles';
+
+export function SignIn() {
+  return (
+    <Container>
+      <Header>
+        <Title>Sign In</Title>
+      </Header>
+    </Container>
+  );
+}
